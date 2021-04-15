91c14b9cba06f569b32203579091005e0185e7b9
Oleksandr <Oleksandr Baior>
2021-04-15 20:20:39 +0300

    add log out and useremail btn on profile page

--- test/pageobjects/profile.page.js
+++ test/pageobjects/profile.page.js
@@ -17,6 +17,12 @@ class ProfilePage {
     get supportPin(){
         return $('//*[@id="ng-app"]/body/div[1]/div/ui-view/div/div[2]/div/div[2]/div/form/div[6]/div[2]/span')
     }
+    get useremail() {
+        return $('//*[@id="ng-app"]/body/div[1]/ssls-header/div/div/header/div/div/div[1]/button/span')
+    }
+    get logOut() {
+        return $('//*[@id="ng-app"]/body/div[1]/ssls-header/div/div/header/div/div/div[1]/div/ul/li[5]/button')
+    }
 }
 
 module.exports = new ProfilePage();
--- test/specs/test.suit.js
+++ test/specs/test.suit.js
@@ -4,6 +4,7 @@ const RandomValue = require('../randomValue');
 const UserPage = require('../pageobjects/user.page');
 const ProfilePage = require('../pageobjects/profile.page');
 
+
 describe('My Login application', () => {
     it('Authorization page. Not registered user TEST_CASE_1', () => {
 // open main page
@@ -20,11 +21,11 @@ describe('My Login application', () => {
 // If user not registered, errors messages such as: “Uh oh! Email or password is incorrect” should be displayed
         expect(Authorize.allert).toBeDisplayed()
     });
-
     it('Authorization page (Welcome back!) TEST_CASE_2', () => {
 //open main page
         MainPage.open()
         MainPage.logInButton.click()
+
 // enter valid email and password for previously registered user
         Authorize.login(Authorize.email,Authorize.password)
 // check entered password, click on "eye” icon in password field
@@ -32,15 +33,19 @@ describe('My Login application', () => {
         expect(Authorize.inputPassword).toHaveAttribute('type', 'text')
         Authorize.loginButton.click()
  // "Log in" button has to be changed on "User@email" button (with dropdown menu) from the left side in the Header of the page
-        expect(UserPage.useremail).toHaveText(email.toUpperCase())
+        expect(UserPage.useremail).toHaveText(Authorize.email.toUpperCase())
         UserPage.useremail.click()
         expect(UserPage.dropdownMenu).toBeDisplayed()
-    });
 
+        UserPage.logOut.click()
+        browser.pause(5000)
+    });
     it(' My profile page. Client area TEST_CASE_3', () => {
 //open main page
         MainPage.open()
+        browser.pause(5000)
         MainPage.logInButton.click()
+
 // enter valid email and password for previously registered user
         Authorize.login(Authorize.email, Authorize.password)
         Authorize.loginButton.click()
@@ -61,7 +66,11 @@ describe('My Login application', () => {
         expect(ProfilePage.phone).toHaveText(valuesProfile[2])
         expect(ProfilePage.address).toHaveText(valuesProfile[3])
         expect(ProfilePage.supportPin).toHaveText(valuesProfile[4])
+
+        ProfilePage.useremail.click()
+        ProfilePage.logOut.click()
     });
 });
+
 
 
