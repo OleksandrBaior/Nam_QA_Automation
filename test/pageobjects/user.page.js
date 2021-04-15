1d469e779fb5d79246e1515b632f4b0ff01cf917
Oleksandr <Oleksandr Baior>
2021-04-15 20:19:26 +0300

    add log out btn

--- test/pageobjects/user.page.js
+++ test/pageobjects/user.page.js
@@ -3,13 +3,16 @@ class UserPage {
      * define selectors using getter methods
      */
     get useremail (){
-        return $('//*[@id="app"]/div/header/div/div/div[1]/button/span')
+        return $('//*[@id="app"]/div/header/div/div/div[1]/button')
     }
     get dropdownMenu(){
         return $('//*[@id="app"]/div/header/div/div/div[1]/div')
     }
     get dropdownProfile(){
         return $('//*[@id="app"]/div/header/div/div/div[1]/div/ul/li[2]/a')
+    }
+    get logOut() {
+        return $('//*[@id="app"]/div/header/div/div/div[1]/div/ul/li[5]')
     }
 }
 
